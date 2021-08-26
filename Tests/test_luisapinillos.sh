5076ae7931ae79346a7da3ea2fd34339690b05b2
LuisaPinillos <luisapinillos20@gmail.com>
2021-08-25 22:35:28 -0500

    Newtest

---
+++ Tests/test_luisapinillos.sh
@@ -0,0 +1,12 @@
+
+echo "/bin/more hsh" | ./hsh
+echo "/bin/more hsh" | sh
+
+echo "/bin/touch hello.c" | ./hsh
+echo "/bin/touch hello.c" | sh
+
+echo "/bin/mv hello.c world.c" | ./hsh
+echo "/bin/mv hello.c world.c" | sh
+
+echo "/bin/rm world.c" | ./hsh
+echo "/bin/rm world.c" | sh
