81a0487e8dc17b43ddcce018c0440701a3b65df2
LuisaPinillos <luisapinillos20@gmail.com>
2021-08-25 22:50:13 -0500

    Other test

--- Tests/test_luisapinillos.sh
+++ Tests/test_luisapinillos.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
 
+#view the text files in the command prompt
 echo "/bin/more hsh" | ./hsh
 echo "/bin/more hsh" | sh
 
+#Create a new file
 echo "/bin/touch hello.c" | ./hsh
 echo "/bin/touch hello.c" | sh
 
+#Change the name of the file
 echo "/bin/mv hello.c world.c" | ./hsh
 echo "/bin/mv hello.c world.c" | sh
 
+#Delate the file
 echo "/bin/rm world.c" | ./hsh
 echo "/bin/rm world.c" | sh
